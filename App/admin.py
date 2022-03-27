4e029498e7d8a750636d898fcad9a81ea45b87e7
Darshan Bhavsar <darshanbhavsar4.db@gmail.com>
2022-03-27 23:14:41 +0530

    Add files via upload

---
+++ App/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import AlumniRegister
+from django.contrib.auth.admin import UserAdmin
+
+
+class AlumniAdmin(UserAdmin):
+	list_display = ('email', 'first_name', 'last_name', 'date_joined', 'last_login')
+	search_fields = ('email', 'first_name', 'last_name')
+	readonly_fields = ('date_joined', 'last_login')
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+
+
+admin.site.register(AlumniRegister, AlumniAdmin)
